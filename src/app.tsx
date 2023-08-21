fb86fc0fd1217d892663448c794d7c5ae22f426d
Arun Sharma <arun2life4084@gmail.com>
2023-08-21 13:57:59 +0530

    Resolved absolute import issue

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { useDarkMode } from "usehooks-ts";
-import { DarkModeToggle, Header } from "./features/ui/components";
+import { DarkModeToggle, Header } from "@features/ui/components";
 
 function App() {
   const { isDarkMode, toggle } = useDarkMode(false);
@@ -8,7 +8,7 @@ function App() {
     <div className={isDarkMode ? "dark" : ""}>
       <Header isDarkMode={isDarkMode} toggle={toggle} />
       <div className="flex h-screen w-full flex-col items-center justify-center gap-4 bg-sky-100 p-8 dark:bg-slate-900 dark:text-white">
-        <div className="bg-skin-fill text-skin-base flex flex-col rounded-xl border-2 border-gray-600 p-8">
+        <div className="flex flex-col rounded-xl border-2 border-gray-600 bg-skin-fill p-8 text-skin-base">
           <h1 className="font-medium">My Portfolio</h1>
           <p>Dark Mode : </p>
           <p>{isDarkMode ? "True" : "False"}</p>
@@ -16,7 +16,7 @@ function App() {
           <DarkModeToggle isDarkMode={isDarkMode} toggle={toggle} />
         </div>
 
-        <div className="bg-skin-fill text-skin-base theme-red flex flex-col rounded-xl border-2 border-gray-600 p-8">
+        <div className="theme-red flex flex-col rounded-xl border-2 border-gray-600 bg-skin-fill p-8 text-skin-base">
           <h1 className="font-medium">My Portfolio</h1>
           <p>Dark Mode : </p>
           <p>{isDarkMode ? "True" : "False"}</p>
